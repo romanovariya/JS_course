afa7526c728b77cf6cc995c8ca4b2c744cdad459
Riya Romanova <riyaromanova13@gmail.com>
2020-11-21 23:41:10 +0300

    feat(script.js): lesson02 easy part

--- js/script.js
+++ js/script.js
@@ -1,10 +1,17 @@
-let money;
-let income;
-let addExpenses;
-let deposit;
-let mission;
-let period;
+const money = 75000,
+    income = 25000,
+    addExpenses = '10000, 5000, 2000',
+    deposit = true,
+    mission = 1000000,
+    period = 8;
+console.log(typeof money);
+console.log(typeof income);
+console.log(typeof deposit);
+console.log(addExpenses.length);
+console.log("Период равен " + period + " месяцев");
+console.log("Цель заработать " + mission + " рублей/долларов/гривен/юани");
+addExpenses.toLocaleLowerCase();
+console.log(addExpenses.split(' '));
 
-alert('Это мой первый урок');
-console.log('Привет, JS!');
-
+let budgetDay = money / 30;
+console.log(budgetDay);
