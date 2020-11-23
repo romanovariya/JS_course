7aa31de0d18cf435a92bb03b09e4ea95f636f927
Riya Romanova <riyaromanova13@gmail.com>
2020-11-23 19:04:51 +0300

    fix(script.js: fixed mistakes

--- js/script.js
+++ js/script.js
@@ -1,28 +1,15 @@
-let money,
-    income = 'рукоделие',
-    addExpenses,
-    deposit,
+const income = 'рукоделие',
     mission = 1000000,
-    period = 8,
-    budgetMonth,
-    budgetDay,
-    expenses1,
-    expenses2,
-    amount1,
-    amount2;
-money = prompt("Ваш месячный доход?");
-addExpenses = prompt("Перечислите возможные расходы за рассчитываемый период через запятую");
-deposit = prompt("Есть ли у вас депозит в банке?")
-if (deposit === "да") {
-    deposit = true;
-} else {
-    deposit = false;
-}
-expenses1 = prompt("Введите обязательную статью расходов?");
-amount1 = prompt("Во сколько это обойдется?");
-expenses2 = prompt("Введите обязательную статью расходов?");
-amount2 = prompt("Во сколько это обойдется?");
-budgetMonth = money - amount1 - amount2;
+    period = 8;
+const money = prompt("Ваш месячный доход?"),
+    addExpenses = prompt("Перечислите возможные расходы за рассчитываемый период через запятую"),
+    deposit = confirm("Есть ли у вас депозит в банке?"),
+    expenses1 = prompt("Введите обязательную статью расходов?"),
+    amount1 = prompt("Во сколько это обойдется?"),
+    expenses2 = prompt("Введите обязательную статью расходов?"),
+    amount2 = prompt("Во сколько это обойдется?"),
+    budgetMonth = money - amount1 - amount2,
+    budgetDay = budgetMonth / 30;
 console.log(budgetMonth);
 console.log(Math.ceil(mission/budgetMonth));
 
@@ -31,7 +18,6 @@ console.log(addExpenses.length);
 console.log("Период равен " + period + " месяцев");
 console.log("Цель заработать " + mission + " рублей");
 console.log(addExpenses.toLowerCase().split(', '));
-budgetDay = budgetMonth / 30;
 console.log((Math.floor(budgetDay)));
 if (budgetDay >= 1200) {
     console.log("У вас высокий уровень дохода");
